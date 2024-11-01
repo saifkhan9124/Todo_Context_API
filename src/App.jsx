20f5a229ec1fa6ab47731b9ba612ecfdc6e5deae
saif9124115@gmail.com <saif9124115@gmail.com.com>
2024-11-01 05:03:20 -0700

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,9 +24,9 @@ function App() {
       value={{ todos, addTodo, upDateTodo, deleteTodo, toggleComplete }}
     >
       <div className="bg-[#172842] min-h-screen py-8">
-        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
-            Manage Your Todo List
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-xl px-4 py-3 text-white">
+          <h1 className="text-3xl font-bold text-center mb-10 mt-4">
+            Manage Your Todo
           </h1>
           <div className="mb-4"></div>
           <div className="flex flex-wrap gap-y-3"></div>
